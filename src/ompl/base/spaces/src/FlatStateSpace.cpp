58c6dee609bb6670306e501dd9a3a179d14e69f7
Clayton Ramsey <claytonwramsey@gmail.com>
2026-09-24 13:04:33 -0500

    docs: update copyright year + add missing notices

--- src/ompl/base/spaces/src/FlatStateSpace.cpp
+++ src/ompl/base/spaces/src/FlatStateSpace.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2017, Rice University
+ *  Copyright (c) 2026, Rice University
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
